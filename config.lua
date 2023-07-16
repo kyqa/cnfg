5f62208ad28a18563c4caaef18022ade516ace51
kyqa <126547641+kyqa@users.noreply.github.com>
2023-07-16 08:08:48 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,11 @@
 function module:GetTeamLogos(teamName)
     if (string.lower(teamName) == "connecticut") then
         return {
-            City = "Connecticut",
-            Name = "Huskies",
-            Abbreviation = "CONN",
             HelmetTexture = "rbxassetid://14082618845",
             Logo = "rbxassetid://14082582244"
         }
     elseif (string.lower(teamName) == "indiana") then
         return {
-            City = "Indiana",
-            Name = "Hoosiers",
-            Abbreviation = "IND",
             HelmetTexture = "rbxassetid://14082618845",
             Logo = "rbxassetid://14082582244"
         }
